1ffe7172c37a54af096c48ec3b42ea0196bc3521
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-11-09 20:19:15 +0700

    add glide

--- app/src/main/java/com/example/json_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/json_kotlin/MainActivity.kt
@@ -3,14 +3,24 @@ package com.example.json_kotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+    lateinit var image: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         playing()
+        loadImage()
+    }
+
+    private fun loadImage(){
+        Glide.with(this)
+            .load("https://i.ibb.co/zJHYGBP/binarlogo.jpg")
+            .into(findViewById(R.id.imgView))
     }
 
     private fun playing() {
