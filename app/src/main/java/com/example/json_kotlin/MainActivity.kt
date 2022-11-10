78cb7a0143305dcf1dce8c01448806a7584bd11d
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-11-10 21:47:02 +0700

    .

--- app/src/main/java/com/example/json_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/json_kotlin/MainActivity.kt
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
                     Log.d("COBAIN", response.toString())
                     Log.e("COBAIN", Gson().toJson(listMovie?.get(0)))
                 }
-                
+
                 override fun onFailure(call: Call<TopMovie>, t: Throwable) {
                     Log.d("YACOBA", t.message.toString())
                 }
