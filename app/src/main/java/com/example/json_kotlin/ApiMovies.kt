b115386ec88a09ed939e8f8b02bbff3b18682c4c
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-11-10 19:26:01 +0700

    Update ApiMovies.kt

--- app/src/main/java/com/example/json_kotlin/ApiMovies.kt
+++ app/src/main/java/com/example/json_kotlin/ApiMovies.kt
@@ -1,17 +1,16 @@
 package com.example.json_kotlin
 
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
 
 class ApiMovies {
     interface Movies {
         @GET("/movie/top_rated")
         fun getMoviesTopRated(@Query("api_key")apiKey: String)
 
-        @POST
+        @POST("/movie/{movie_id}/rating")
+        fun postRate(@Path("movie_id") movie_id: String)
 
-
-
+        @DELETE
+        fun funt()
     }
 }
