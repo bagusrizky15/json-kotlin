d9bd4cf159d90fd2f04904d8d04bdca20fe3fa6e
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-11-15 20:10:02 +0700

    Update MovieAdapter.kt

--- app/src/main/java/com/example/json_kotlin/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/json_kotlin/adapter/MovieAdapter.kt
@@ -7,11 +7,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.json_kotlin.R
 import com.example.json_kotlin.model.TopMovie
 
-class MovieAdapter(val results: ArrayList<TopMovie>) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
-    class ViewHolder (val view: View) : RecyclerView.ViewHolder(view)
+class MovieAdapter(val results: ArrayList<TopMovie>
+) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder (
-        val
+        val binding =
         LayoutInflater.from(parent.context).inflate(R.layout.item_movies, parent, false)
     )
 
@@ -20,5 +21,7 @@ class MovieAdapter(val results: ArrayList<TopMovie>) : RecyclerView.Adapter<Movi
     }
 
     override fun getItemCount() = results.size
+
+    class ViewHolder (val view: View) : RecyclerView.ViewHolder(view)
 
 }
