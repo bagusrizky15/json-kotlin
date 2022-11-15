46c5fe1a2fe7ce78f404db3e20d9a657019166b4
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-11-15 20:41:50 +0700

    .

--- app/src/main/java/com/example/json_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/json_kotlin/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.json_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.json_kotlin.adapter.MovieAdapter
 import com.example.json_kotlin.api.ApiClient
 import com.example.json_kotlin.api.ApiKey
+import com.example.json_kotlin.databinding.ActivityMainBinding
+import com.example.json_kotlin.model.TopMovie
 import com.example.json_kotlin.model.TopMovieResponse
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: MovieAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = MovieAdapter(arrayListOf(), this@MainActivity)
+        binding.recycleView.layoutManager = LinearLayoutManager(this)
+        binding.recycleView.adapter = adapter
         fetchAllData()
     }
 
@@ -21,8 +31,9 @@ class MainActivity : AppCompatActivity() {
         ApiClient.apiClient.getMoviesTopRated(ApiKey.apiKeyMovieDB)
             .enqueue(object : Callback<TopMovieResponse>{
                 override fun onResponse(call: Call<TopMovieResponse>, response: Response<TopMovieResponse>) {
-
-                    //val listMovie = movie?.results
+                    val movie = response.body()
+                    val listMovie = movie?.results
+                    setData(listMovie)
 //                    Log.d("COBAIN", response.toString())
 //                    Log.e("COBAIN", Gson().toJson(listMovie?.get(0)))
                 }
@@ -31,6 +42,10 @@ class MainActivity : AppCompatActivity() {
                    // Log.d("YACOBA", t.message.toString())
                 }
             })
+    }
+
+    private fun setData(result: ArrayList<TopMovie?>?) {
+        adapter.setData(result)
     }
 
 }
--- app/src/main/java/com/example/json_kotlin/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/json_kotlin/adapter/MovieAdapter.kt
@@ -9,7 +9,7 @@ import com.example.json_kotlin.R
 import com.example.json_kotlin.databinding.ItemMoviesBinding
 import com.example.json_kotlin.model.TopMovie
 
-class MovieAdapter(val results: ArrayList<TopMovie>, val context: Context
+class MovieAdapter(val result: ArrayList<TopMovie>, val context: Context
 ) : RecyclerView.Adapter<MovieAdapter.ViewHolder>() {
     class ViewHolder (view: View) : RecyclerView.ViewHolder(view){
         val binding = ItemMoviesBinding.bind(view)
@@ -22,10 +22,18 @@ class MovieAdapter(val results: ArrayList<TopMovie>, val context: Context
     )
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.itemView
+        holder.tvTitle.text = result[position].title
+        holder.tvDesc.text = result[position].overview
     }
 
-    override fun getItemCount() = results.size
+    override fun getItemCount() = result.size
+
+    fun setData(result: ArrayList<TopMovie?>?){
+        result?.clear()
+        result?.addAll(result)
+        notifyDataSetChanged()
+    }
+
 
 
 
