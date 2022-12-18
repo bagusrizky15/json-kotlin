50d5c9a6e8a8d6596cd4a5601e98bd5c239dd4d3
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-12-18 16:01:36 +0700

    logout

--- app/src/main/java/com/example/json_kotlin/ui/ProfileActivity.kt
+++ app/src/main/java/com/example/json_kotlin/ui/ProfileActivity.kt
@@ -1,12 +1,30 @@
 package com.example.json_kotlin.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.json_kotlin.R
+import android.widget.Toast
+import com.example.json_kotlin.databinding.ActivityProfileBinding
+import com.example.json_kotlin.helper.PreferenceHelper
 
 class ProfileActivity : AppCompatActivity() {
+    lateinit var binding: ActivityProfileBinding
+    private lateinit var sharedPreferences: PreferenceHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sharedPreferences = PreferenceHelper(this)
+        binding.btnLogout.setOnClickListener{
+            sharedPreferences.clear()
+            Toast.makeText(applicationContext, "Berhasil Keluar", Toast.LENGTH_SHORT).show()
+            moveIntent()
+        }
+    }
+
+    private fun moveIntent() {
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
