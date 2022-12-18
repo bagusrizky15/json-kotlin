b45f93f51ed74ca09b6dcf18dd754ab725aacc36
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-12-18 12:14:36 +0700

    register

--- app/src/main/java/com/example/json_kotlin/ui/LoginActivity.kt
+++ app/src/main/java/com/example/json_kotlin/ui/LoginActivity.kt
@@ -1,12 +1,22 @@
 package com.example.json_kotlin.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.json_kotlin.MainActivity
 import com.example.json_kotlin.R
+import com.example.json_kotlin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener{
+            val i = Intent(this, MainActivity::class.java)
+            startActivity(i)
+        }
     }
 }
