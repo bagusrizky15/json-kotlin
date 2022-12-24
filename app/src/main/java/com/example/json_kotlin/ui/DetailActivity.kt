b7642c702963982e8ba8f6515624d3d515d340bf
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-12-24 16:19:44 +0700

    Update DetailActivity.kt

--- app/src/main/java/com/example/json_kotlin/ui/DetailActivity.kt
+++ app/src/main/java/com/example/json_kotlin/ui/DetailActivity.kt
@@ -2,11 +2,14 @@ package com.example.json_kotlin.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.json_kotlin.MainActivity
 import com.example.json_kotlin.R
+import com.example.json_kotlin.model.TopMovie
 
 class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        
     }
 }
