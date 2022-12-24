d0e9bd6b287bebf94c9d5ff65987c618208cfc73
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-12-24 16:15:42 +0700

    .

--- app/src/main/java/com/example/json_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/json_kotlin/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var adapter: MovieAdapter
     lateinit var sharedPreferences : PreferenceHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
