dd66cbdd13d120e5b2f917b46880e0a41899d2ca
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-12-28 21:27:12 +0700

    6

--- app/src/main/java/com/example/json_kotlin/ui/LoginActivity.kt
+++ app/src/main/java/com/example/json_kotlin/ui/LoginActivity.kt
@@ -9,8 +9,12 @@ import com.example.json_kotlin.MainActivity
 import com.example.json_kotlin.databinding.ActivityLoginBinding
 import com.example.json_kotlin.helper.Constant
 import com.example.json_kotlin.helper.PreferenceHelper
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     lateinit var binding: ActivityLoginBinding
     lateinit var sharedPreferences : PreferenceHelper
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +22,7 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = Firebase.auth
         binding.btnDaftar.setOnClickListener{
             val j = Intent(this, RegisterActivity::class.java)
             startActivity(j)
@@ -36,9 +41,17 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser!= null){
+            reload()
+        }
         if (sharedPreferences.getBoolean(Constant.PREF_IS_LOGIN)){
             moveIntent()
         }
+    }
+
+    private fun reload() {
+        TODO("Not yet implemented")
     }
 
     private fun moveIntent() {
