dda98c33779cc8813b511a4bcdaf12c185813ed5
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-12-30 21:51:06 +0700

    Update RegisterActivity.kt

--- app/src/main/java/com/example/json_kotlin/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/json_kotlin/ui/RegisterActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import android.widget.Toast
 import com.example.json_kotlin.MainActivity
 import com.example.json_kotlin.databinding.ActivityRegisterBinding
@@ -15,6 +16,8 @@ import com.google.firebase.ktx.Firebase
 class RegisterActivity : AppCompatActivity() {
     lateinit var binding: ActivityRegisterBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var semail: EditText
+    private lateinit var sPassword: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
