ca2267c1cdc501afda86186ee30c7ea32c9a8f2c
mohamad.15@students.amikom.ac.id <mohamad.15@students.amikom.ac.id>
2022-12-31 15:52:44 +0700

    unit test

--- app/src/test/java/com/example/json_kotlin/ExampleUnitTest.kt
+++ app/src/test/java/com/example/json_kotlin/ExampleUnitTest.kt
@@ -14,4 +14,14 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun multiply_test(){
+        val a = 10
+        val b = 2
+        val expectedResult = 20
+        assertEquals(expectedResult, multiply(a,b))
+    }
+
+    fun multiply(a: Int, b: Int) = a * b
 }
---
+++ app/src/test/java/com/example/json_kotlin/api/ApiKeyTest.kt
@@ -0,0 +1,10 @@
+package com.example.json_kotlin.api
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class ApiKeyTest {
+    @Test
+
+
+}
